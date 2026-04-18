002c0a1d20b4d99ac2d8fd471c786f62dd9848fa
fran utibe joseph <majesticfrank7@gmail.com>
2026-04-18 02:13:58 +0100

    settings

--- IntelligentProfile/urls.py
+++ IntelligentProfile/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    re_path(r"^api/?/", include("profiles.urls")),
-    re_path(r"^api/?", include("profiles.urls")),
+    path("api/", include("profiles.urls")),
 ]
